30136c6deda75e034474f0f49b7ab6507f113723
Salavat <sshirgaleev@icloud.com>
2019-07-09 16:36:40 -0400

    postgres dependancy

---
+++ src/main/java/com/pawntracker/Entity/User.java
@@ -0,0 +1,32 @@
+package com.pawntracker.Entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String username;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
