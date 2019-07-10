9a445d5b5e69f660640ed1125e3f3948becdfd7e
Salavat <sshirgaleev@icloud.com>
2019-07-10 15:05:29 -0400

    navigation bar fragmentation

--- src/main/java/com/pawntracker/controller/IndexController.java
+++ src/main/java/com/pawntracker/controller/IndexController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HomeController {
+public class IndexController {
     @GetMapping("/")
-    public String homePage(@RequestParam(name="name", required=false, defaultValue = "World!") String name, Model model ) {
+    public String indexPage(@RequestParam(name="name", required=false) String name, Model model ) {
+        name = "Pawn & consignment shop tracker application";
         model.addAttribute("name", name);
-        return "home";
+        return "index";
     }
 }
