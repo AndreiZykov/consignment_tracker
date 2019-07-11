63224b07368ab094cd04f29532ae0d5561c8617d
Salavat Shirgaleev <43363555+sshirgaleev@users.noreply.github.com>
2019-07-11 00:22:23 -0400

    Task2 (#4)

---
+++ src/main/java/com/pawntracker/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.pawntracker.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String indexPage(@RequestParam(name="name", required=false) String name, Model model ) {
+        name = "Pawn & consignment shop tracker application";
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
