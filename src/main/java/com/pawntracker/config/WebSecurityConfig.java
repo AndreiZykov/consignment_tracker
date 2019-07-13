f5a6da0dd0dd4a24231a724c9892b055745dcddc
Salavat <sshirgaleev@icloud.com>
2019-07-12 20:36:07 -0400

    logout configs

--- src/main/java/com/pawntracker/config/WebSecurityConfig.java
+++ src/main/java/com/pawntracker/config/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -49,8 +50,9 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
                         .loginPage("/login")
                         .permitAll()
                         .and()
-                    .logout()
-                        .permitAll().logoutSuccessUrl("/");
+                    .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+              .logoutSuccessUrl("/login").deleteCookies("JSESSIONID")
+              .invalidateHttpSession(true) ;
     }
 
     @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
