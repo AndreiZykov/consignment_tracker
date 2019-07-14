459ea890ca8a38e4fd47f9ddcca722b319ac269d
Salavat <sshirgaleev@icloud.com>
2019-07-13 20:47:13 -0400

    login form ui/ux

--- src/main/java/com/pawntracker/controller/UserController.java
+++ src/main/java/com/pawntracker/controller/UserController.java
@@ -55,13 +55,12 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public String login(Model model, String error, String logout) {
+    public String login(  Model model, String error, String logout) {
         if (error != null)
             model.addAttribute("error", "Your username and password is invalid.");
-        System.out.println("Your username and password is invalid.");
+
         if (logout != null)
             model.addAttribute("message", "You have been logged out successfully. ");
-        System.out.println("You have been logged out successfully.");
 
         return "login";
     }
