0e9e4ebe6b2d9dfc7c71ec4af5fb3886eb6b1bb4
Salavat <sshirgaleev@icloud.com>
2019-08-06 13:59:35 -0400

    item controller tests

--- src/test/java/com/pawntracker/controller/ItemControllerTest.java
+++ src/test/java/com/pawntracker/controller/ItemControllerTest.java
@@ -1,33 +1,19 @@
 package com.pawntracker.controller;
 
-import com.pawntracker.entity.Item;
-import com.pawntracker.repository.ItemRepository;
 import com.pawntracker.service.ItemService;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import javax.swing.*;
 
-import java.util.ArrayList;
-
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ItemController.class)
@@ -36,19 +22,54 @@ public class ItemControllerTest {
     @MockBean
     ItemService itemService;
 
-
-
-
     @Autowired
     MockMvc mockMvc;
-
-
 
     @Test
     public void itemList() throws Exception {
         RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/items/all");
-
         MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-
+        System.out.println(result);
     }
+
+    @Test
+    public void getItem() throws Exception {
+        Long id = 1L;
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/items/" + id);
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        System.out.println(result);
+    }
+
+
+    @Test
+    public void createForm() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/items/create");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        System.out.println(result);
+    }
+
+    @Test
+    public void saveNewItem() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/items/create"  );
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        System.out.println(result);
+    }
+
+    @Test
+    public void myItemList() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/myitems"  );
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        System.out.println(result);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        Long id = 1L;
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/delete/item" + id  );
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        System.out.println(result);
+    }
+
+
+
 }
