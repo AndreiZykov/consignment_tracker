0a1a9c01afeac6565d0796ba2d7a9c145360cb90
Salavat <sshirgaleev@icloud.com>
2019-08-06 13:46:13 -0400

    item controller itemList() method test

--- src/test/java/com/pawntracker/controller/ItemControllerTest.java
+++ src/test/java/com/pawntracker/controller/ItemControllerTest.java
@@ -1,34 +1,54 @@
 package com.pawntracker.controller;
 
+import com.pawntracker.entity.Item;
+import com.pawntracker.repository.ItemRepository;
 import com.pawntracker.service.ItemService;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import javax.swing.*;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(ItemController.class)
 public class ItemControllerTest {
 
     @MockBean
     ItemService itemService;
 
+
+
+
+    @Autowired
     MockMvc mockMvc;
+
+
 
     @Test
     public void itemList() throws Exception {
-        mockMvc.perform(get("/items/all"))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeExists("items"))
-                .andExpect(model().attributeExists("text"))
-                .andExpect(view().name("items/all_items"));
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/items/all");
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
     }
 }
